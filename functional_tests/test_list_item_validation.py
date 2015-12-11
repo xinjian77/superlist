a851953c49fe7fb25cc0c9285b2728fcc3f9b880
localadmin <localadmin@webdev>
2015-12-11 23:30:29 +0800

    Adjust new list view to do model validation

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -3,7 +3,7 @@ from unittest import skip
 
 class ItemValidationTest(FunctionalTest):
 
-    @skip
+#    @skip
     def test_cannot_add_empty_list_items(self):
         #伊迪丝访问首页，不小心提交了一个空待办事项
         #输入框中没输入内容，她就按下了回车键
@@ -13,7 +13,7 @@ class ItemValidationTest(FunctionalTest):
         #首页刷新了，显示一个错误信息
         #提示待办事项不能为空
         error =  self.browser.find_element_by_css_selector('.has-error')
-        self.assertEuqal(error.text, "You can't have an empty list item")
+        self.assertEqual(error.text, "You can't have an empty list item")
         
         #她输入一些文字，然后再次提交，这次没问题了
         self.browser.find_element_by_id('id_new_item').send_keys('Buy milk\n')
