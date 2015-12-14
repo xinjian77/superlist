b0feddf868d3f3895a656f070f1f6b2f50636d1a
localadmin <localadmin@webdev>
2015-12-14 13:21:28 +0800

    chap11l022

--- lists/forms.py
+++ lists/forms.py
@@ -17,11 +17,3 @@ class ItemForm(forms.models.ModelForm):
         error_messages = {
                 'text': {'required': EMPTY_LIST_ERROR}
         }
-
-# class ItemForm(forms.Form):
-#     item_text = forms.CharField(
-#             widget = forms.fields.TextInput(attrs={
-#                     'placeholder': 'Enter a to-do item',
-#                     'class': 'form-control input-lg',
-#             }),
-#     )
