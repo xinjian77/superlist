f71586802926b9b82f375ace16b2dedd89372eef
localadmin <localadmin@webdev>
2015-12-14 12:35:58 +0800

    ch11l012

---
+++ lists/tests/test_forms.py
@@ -0,0 +1,14 @@
+from django.test import TestCase
+
+from lists.forms import ItemForm
+
+class ItemFormTest(TestCase):
+    
+    def test_form_item_input_has_placeholder_and_css_classes(self):
+        form = ItemForm()
+        self.assertIn('placeholder="Enter a to-do item"', form.as_p())
+        self.assertIn('class="form-control input-lg"', form.as_p())
+    
+#     def test_form_renders_item_text_input(self):
+#         form = ItemForm()
+#         self.fail(form.as_p())
